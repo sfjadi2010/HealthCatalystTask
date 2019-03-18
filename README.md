924b98bcd4649395fcd1e8d11fb4f47eb8cea41f
Salman Farsi <salman@acupera.com>
2019-03-17 22:48:28 -0400

    Initialize

---
+++ README.md
@@ -0,0 +1,1 @@
+# .NET Core | Postgres | Docker | Angular
---
+++ Server/PersonSearch/Models/PersonRecord.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonSearch.Models
+{
+    public class PersonRecord
+    {
+        public Person Person { get; set; }
+        public Address Address { get; set; }
+        public Picture Picture { get; set; }
+    }
+}
