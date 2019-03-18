156b254827d425d94f37ad92f3655d47b61e6c52
Salman Farsi <salman@acupera.com>
2019-03-17 23:09:18 -0400

    added ef and cors

---
+++ Server/PersonSearch/Context/ApiDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using PersonSearch.Models;
+
+namespace PersonSearch.Context
+{
+    public class ApiDbContext: DbContext
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options) { }
+
+        public DbSet<Person> People { get; set; }
+    }
+}
